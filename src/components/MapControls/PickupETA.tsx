9e2f42055b46a7ade25246f1e47024f3707a43b5
Dyllon <dyllon.g@outlook.com>
2021-05-25 09:23:07 +0800

    disabled user-select css

--- src/components/MapControls/PickupETA.tsx
+++ src/components/MapControls/PickupETA.tsx
@@ -24,6 +24,7 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  user-select: none;
   box-shadow: rgba(17, 17, 26, 0.1) 0px 1px 0px,
     rgba(17, 17, 26, 0.1) 0px 8px 24px,
     rgba(17, 17, 26, 0.1) 0px 16px 48px;
